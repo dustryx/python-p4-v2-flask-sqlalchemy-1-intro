faa5e8a5448074758f84a7598befacce99317301
Alex Karanja <alexnjorge953@gmail.com>
2024-06-26 14:48:00 +0300

    sqlalchemy flask updates

--- server/models.py
+++ server/models.py
@@ -16,3 +16,24 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True,
+                         nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True)
+    verified = db.Column(db.Boolean, default=False)
+
+
+
+# class Dog(db.Model):
+#     __tablename__= 'dogs'
+
+#     id = db.Column(db.integer, prmary_key=True)
+#     name =db.Column(db.String, nullable = False)
+#     owner =db.Column(db.String, nullable = False)
